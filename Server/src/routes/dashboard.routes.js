33b1e6178d6f54b05a6751d871f9104eded2df7c
Lishika3009 <129992113+Lishika3009@users.noreply.github.com>
2025-06-28 17:29:08 +0530

    Dashboard and role

--- Server/src/routes/dashboard.routes.js
+++ Server/src/routes/dashboard.routes.js
@@ -1,5 +1,6 @@
 const router = require("express").Router();
 const { verifyToken } = require("../middleware/oauth");
+const { checkRole } = require("../middleware/role.middleware");
 const ctrl = require("../controllers/dashboard.controller");
 
 router.use(verifyToken);
@@ -9,5 +10,15 @@ router.patch("/profile", ctrl.updateProfile);
 
 router.get("/events", ctrl.userEvents);
 router.get("/activity", ctrl.recentActivity);
+
+// Event Organizer Routes
+router.post("/create-event", checkRole(["event organizer"]), ctrl.createEvent);
+router.put("/edit-event/:id", checkRole(["event organizer"]), ctrl.editEvent);
+router.patch("/manage-event-details/:id", checkRole(["event organizer"]), ctrl.manageEventDetails);
+router.get("/participants/:id", checkRole(["event organizer"]), ctrl.viewParticipants);
+router.put("/approve-participant/:id", checkRole(["event organizer"]), ctrl.approveParticipant);
+router.get("/export-participants/:id", checkRole(["event organizer"]), ctrl.exportParticipants);
+router.get("/event-analytics/:id", checkRole(["event organizer"]), ctrl.getAnalytics);
+
 
 module.exports = router;
